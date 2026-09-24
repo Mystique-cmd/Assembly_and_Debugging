7ee0f52303664f7dc6320f5a1135bbebecd1d753
Mystique <mystiquesoul777@gmail.com>
2026-09-24 18:32:10 -0400

    Docs: Read Syscall Lab

---
+++ Shellcode Crafting/Shellcode Lab/03_read/shellcode.asm
@@ -0,0 +1,24 @@
+global _start
+section .text
+_start:
+	;----read(0, buffer, 64)
+	mov rax, 0		;sys_read
+	mov rdi, 0		;stdin
+	mov rsi, buffer
+	mov rdx, 64
+	syscall
+	
+	;----write(1, buffer, 64)
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, buffer
+	mov rdx, 64
+	syscall
+	
+	;---exit(0(
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+	
+buffer:
+	times 64 db 0
